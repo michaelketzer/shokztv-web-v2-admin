ed0af4b17fc0ca19062c27c4ee52cd9dbc953ccd
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-02-12 20:38:32 +0100

    Added description to add form for tags;

--- src/pages/tags/AddTag.tsx
+++ src/pages/tags/AddTag.tsx
@@ -2,21 +2,24 @@ import React, { ReactElement, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Row, Button, Modal, Input } from 'antd';
 import { createTag } from '../../store/Tag';
+import TextArea from 'antd/lib/input/TextArea';
 
 export default function AddTag(): ReactElement {
   const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
   const [tagName, setTagName] = useState('');
+  const [description, setDescription] = useState('');
   const [image, setImage] = useState<File | null>(null);
   const [loading, setLoading] = useState(false);
 
   const onCreate = async () => {
       if(tagName.length) {
         setLoading(true);
-        await dispatch(createTag(tagName, image));
+        await dispatch(createTag(tagName, description, image));
         setLoading(false);
         setShowModal(false);
         setTagName('');
+        setDescription('');
         setImage(null);
       }
   }
@@ -37,6 +40,10 @@ export default function AddTag(): ReactElement {
                 disabled={loading}
                 onChange={(e) => setTagName(e.target.value)}
                 onPressEnter={onCreate} />
+
+        <div style={{margin: '10px 0'}} />
+
+        <TextArea placeholder={'Description'} id="description" value={description} onChange={({target}) => setDescription(target.value)}/>
 
         <div style={{margin: '10px 0'}} />
 
