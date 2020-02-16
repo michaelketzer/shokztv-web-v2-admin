608e8e3859bd7cb0d5d2fbeb47ee2e2937defc9f
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-02-16 15:48:23 +0100

    Added editing of organizer and deletion;

--- src/@types/Entities/Organizer.d.ts
+++ src/@types/Entities/Organizer.d.ts
@@ -4,4 +4,5 @@ export interface Organizer {
     description: string;
     icon: string;
     logo: string;
+    events: number;
 }
--- src/pages/organizer/OrganizerList.tsx
+++ src/pages/organizer/OrganizerList.tsx
@@ -1,15 +1,22 @@
-import { ReactElement, useEffect } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadOrganizer } from "../../store/Organizer";
+import { loadOrganizer, patchOrganizer, deleteOrganizer } from "../../store/Organizer";
 import { organizerSelector } from "../../store/selectors/organizer";
-import { Table } from "antd";
+import { Table, Button, Popconfirm, Modal, Form, Input } from "antd";
+import ButtonGroup from "antd/lib/button/button-group";
+import FileForm from "../components/FileForm";
 
 const renderImage = (src) => <img src={`${process.env.API_URL}${src}`} alt='organization-logo'/>
-const columns = [
+const getColumns = (onDelete: (id: number) => Promise<void>, onEdit: (id: number) => void) => [
   {
     title: 'Name',
     dataIndex: 'name',
     key: 'name',
+  },
+  {
+    title: 'Events',
+    dataIndex: 'events',
+    key: 'events',
   },
   {
     title: 'Icon',
@@ -23,6 +30,16 @@ const columns = [
     key: 'logo',
     render: (text) => renderImage(text),
   },
+  {
+    title: '',
+    key: 'actions',
+    render: (text, record) => <ButtonGroup>
+      <Button icon='edit' onClick={() => onEdit(record.id)} />
+      <Popconfirm title="Sure to delete?" onConfirm={() => this.onDelete(record.id)} disabled={record.events > 0}>
+        <Button type="danger" icon='delete' disabled={record.events > 0} />
+      </Popconfirm>
+    </ButtonGroup>,
+  },
 ];
 
 export default function OrganizerList(): ReactElement {
@@ -33,5 +50,49 @@ export default function OrganizerList(): ReactElement {
         dispatch(loadOrganizer());
     },[]);
 
-    return <Table dataSource={organizers} columns={columns} rowKey={'id'}/>;
+    const [id, setId] = useState(0);
+    const [name, setName] = useState('');
+    const [icon, setIcon] = useState<string | File | null>(null);
+    const [logo, setLogo] = useState<string | File | null>(null);
+    const [showModal, setShowModal] = useState(false);
+    const [loading, setLoading] = useState(false);
+    
+    const onEdit = (id: number) => {
+      const entry = organizers.find(({id: rowId}) => rowId === id);
+      setId(id);
+      setName(entry.name);
+      setIcon(entry.icon);
+      setLogo(entry.logo);
+      setShowModal(true);
+    };
+
+    const onPatch = async () => {
+      setLoading(true);
+      await dispatch(patchOrganizer(id, name, icon instanceof File ? icon : undefined, logo instanceof File ? logo : undefined));
+      setLoading(false);
+      setShowModal(false);
+      setName('');
+      setIcon(null);
+      setLogo(null);
+    }
+
+    const onDelete = async (id: number) => {
+      await dispatch(deleteOrganizer(id));
+    }
+
+    const columns = getColumns(onDelete, onEdit);
+
+    return <>
+      <Table dataSource={organizers} columns={columns} rowKey={'id'}/>
+      <Modal title="Edit organizer" visible={showModal} onOk={onPatch} onCancel={() => setShowModal(false)} confirmLoading={loading}>
+        <Form layout={'vertical'}>
+          <Form.Item label={'Name'}>
+              <Input id="name" type="text" value={name} onChange={({target}) => setName(target.value)}/>
+          </Form.Item>
+
+          <FileForm file={icon} setFile={setIcon} label={'Icon'} />
+          <FileForm file={logo} setFile={setLogo} label={'Logo'} />
+        </Form>
+      </Modal>
+    </>;
 }
