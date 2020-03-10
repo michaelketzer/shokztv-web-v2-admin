bcd6d4c08d8f33f720a677bf9daf4e51aaa08a64
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-10 18:29:58 +0100

    Upgraded libs;

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 
-const withCSS = require('@zeit/next-css');
-
-module.exports = withCSS({
+module.exports = {
     env: {
         API_URL: process.env.API_URL
     },
-});
+};
