51d5ec4102c22c07c33927eec8362f602bb8a4b8
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-11 22:11:07 +0100

    Added spacer for menu; Added console view;

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,17 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useMemo } from 'react';
 import PageMenu from './components/PageMenu';
 import Head from 'next/head';
 import Layout from 'antd/lib/layout';
-const { Header } = Layout;
+import { useSelector } from 'react-redux';
+import { currentUserSelector } from '../store/selectors/currentuser';
+import { Result, Button } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
+const { Content, Header } = Layout;
 
 export default function Dashboard(): ReactElement {
+  const currentUser = useSelector(currentUserSelector);
+
+  const hasRoles = useMemo(() => currentUser && currentUser.roles.length > 0, [currentUser]);
   return <Layout className="layout" style={{ height: '100vh' }}>
     <Head>
       <title>Dashboard</title>
@@ -12,5 +19,12 @@ export default function Dashboard(): ReactElement {
     </Head>
 
     <Header  style={{ height: '46px' }}><PageMenu /></Header>
+
+    <Content style={{ padding: '50px', overflowY: 'scroll'}}>
+      {!hasRoles &&  <Result
+        icon={<SmileOutlined />}
+        title="Yeah! Dein Boarding beginnt gerade. Wir müssen dir nur noch eine maßgeschneiderte Rolle zuweisen. Melde dich doch einfach im Discord, dass du es hierhin geschafft hast."
+      />}
+    </Content>
   </Layout>;
 }
